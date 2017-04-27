47ba0910689bda34ddfb2b6a42caa48402492583
Rei Matsuzaki (rccls) <matsuzaki.rei@sepia.chem.keio.ac.jp>
2017-04-27 15:24:32 +0900

    add some calculation

---
+++ calc/h2_rpa/10s6p__p__pf/run.sh
@@ -0,0 +1,4 @@
+../../../one_eig/bin/fast/one_eig oneeig.in.json  > oneeig.out
+../../../rhf/bin/fast/rhf rhf.in.json  > rhf.out
+../../../two_pot/bin/fast/two_pot two_pot.in.json > two_pot.out
+
---
+++ calc/h2_stex/10s6p__pf__pf_s/for_fixed_mole/run.sh
@@ -0,0 +1,2 @@
+../../../../two_pot/bin/fast/two_pot two_pot.in.json | tee two_pot.out.json
+
